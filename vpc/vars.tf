764761c7b475918c829dc5f77f3970dd1554cb8c
saivulavapati <saivulavapati@gmail.com>
2025-01-03 09:23:01 +0000

    provider and variables created

---
+++ vpc/vars.tf
@@ -0,0 +1,11 @@
+#variables
+
+variable "aws_secert_key" {
+  type        = string
+  description = "Enter Your Secert Key"
+}
+
+variable "aws_access_key" {
+  type        = string
+  description = "Enter Your access Key"
+}
